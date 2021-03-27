1c1ddfc200d615f6269de69d09aed7f00497adb6
Md Firoj Kabir <firoj.kabir65@gmail.com>
2021-03-27 19:47:20 +0100

    Create iceCream components for body

---
+++ src/components/iceCream/iceCream.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import classes from './iceCream.module.css'
+import Scoop from './Scoop/Scoop'
+
+const iceCream = () => {
+    return (
+        <div>
+            <div className={classes.icecream}>
+                <p className={classes.cone}></p>
+                {/* <p>Please start adding scoops</p> */}
+                <Scoop />
+                <div className={classes.cherry}></div>
+            </div>
+        </div>
+    )
+}
+
+export default iceCream
